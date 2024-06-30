792d51b542a277b9817c4d1b69082a49a2886597
Laith <laith.youcef11@gmail.com>
2024-06-30 22:35:05 +0100

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHero(id: number):Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+
+  }
+
+  constructor(private messageService:MessageService) { }
+  getHeroes():Observable<Hero[]>{
+    this.messageService.add('HeroService: fetched heroes')
+    return of( HEROES);
+  }
+}
